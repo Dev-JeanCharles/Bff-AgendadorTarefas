ec7000b86c0641fb6a0c7d7c9b3bcbce8c382dad
Dev-JeanCharles <jeancduasan@gmail.com>
2026-05-03 20:25:41 -0300

    :sparkles: feat: Adicionando Cors

--- src/main/java/com/javanauta/bffagendadortarefas/infrastructure/configs/CorsConfig.java
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/configs/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.javanauta.bffagendadortarefas.infrastructure.configs;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
